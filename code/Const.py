e2ac442b8731572a9a7b0da8c197fbd91e887b2a
michelcorrica <michelcorrica36@gmail.com>
2025-03-29 19:08:58 -0300

    Creatin Menu and Game w pics and sounds

---
+++ code/Const.py
@@ -0,0 +1,9 @@
+from pygame.examples.grid import WINDOW_HEIGHT
+
+MENU_OPTION = ('NOVO JOGO',
+               'NOVO JOGO 2P',
+               'SCORE',
+               'SAIR'
+               )
+WIN_WIDTH = 576
+WIN_HEIGHT = 360
--- code/Game.py
+++ code/Game.py
@@ -1,22 +1,20 @@
 import pygame
 
+from code.Const import WIN_HEIGHT, WIN_WIDTH
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
 
-            # check for all events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()  # close
-                    quit()  # end pygame
+
 
