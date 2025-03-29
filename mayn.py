c2a8a478073d8fef5fc007ea21be9ffcdc5e73b1
michelcorrica <michelcorrica36@gmail.com>
2025-03-29 18:13:23 -0300

    Creatin Menu and Game

--- mayn.py
+++ mayn.py
@@ -1,14 +1,4 @@
-import pygame
-print('Setup Start')
-pygame.init()
-window = pygame.display.set_mode(size = (600,480))
-print('Setup End')
+from code.Game import Game
 
-print('Loop start')
-while True:
-    #check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #close
-            quit() #end pygame
-
+game = Game()
+game.run()
