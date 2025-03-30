b3172c211b20abd569b0dc1be891123965db8057
michelcorrica <michelcorrica36@gmail.com>
2025-03-30 09:18:17 -0300

    Changing color menu

--- code/Menu.py
+++ code/Menu.py
@@ -17,8 +17,8 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50,'Corrica',(255,180,4),((WIN_WIDTH/2),70))
-            self.menu_text(50, 'Game', (255, 180, 4), ((WIN_WIDTH / 2), 120))
+            self.menu_text(50,'Corrica',(15,240,150),((WIN_WIDTH/2),70))
+            self.menu_text(50, 'Game', (15, 240,150), ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 self.menu_text(25,MENU_OPTION[i],(255,255,255),((WIN_WIDTH / 2), 200+30*i))
